808b2495031f6531ff25a003ccbe5e974b01d05f
pairsupit <pair.supitsara@gmail.com>
2023-08-19 00:38:25 +0700

    create class loadmore
    create class for readable and reusable code

--- script/category.js
+++ script/category.js
@@ -1,30 +1,30 @@
-const btBestSeller = document.getElementById("loadBestSellerMore")
+
+import { loadmore } from "./module/loadmore.js"
+
+let itemsBestSeller = [0,1,2,3,4,5,6,7,8,9]
+let items2 = [0,1]
+let items3 = [0,1,2]
+
 const divBestSeller = document.getElementById("bestSeller")
-
-let items = [0,1,2,3,4,5,6,7,8,9]
-let lastIndex =  items.length
-let startIndex = 0
-const ItemPerload = 4
-
-/* initial */
-fnLoadItems()
-
-btBestSeller.addEventListener('click', function () {
-    fnLoadItems()
+const btBestSeller = document.getElementById("loadBestSellerMore")
+const load1 = new loadmore(items, divBestSeller)
+load1.init()
+btBestSeller.addEventListener('click', function(){
+    load1.fnLoadItems()
 })
-function fnLoadItems() {
-    let endIndex = startIndex + ItemPerload
-    for(let i = startIndex ; i < endIndex && i < lastIndex ; i++) {
-        fnCreateBox(i)
-    }    
-    startIndex += ItemPerload;
-}
 
-function fnCreateBox(index) {
-    const el = document.createElement("div");
-    const attr = document.createAttribute("class");    
-    attr.value = "box"; /* class box */
-    el.innerText = "index" + index;  
-    el.setAttributeNode(attr);
-    divBestSeller.appendChild(el)
-}
+const divOnSale = document.getElementById("onSale")
+const btOnSale = document.getElementById("loadOnSaleMore")
+const load2 = new loadmore(items2, divOnSale)
+load2.init()
+btOnSale.addEventListener('click', function(){
+    load2.fnLoadItems()
+})
+
+const divNewArrive = document.getElementById("NewArrive")
+const btNewArrive = document.getElementById("loadNewArriveMore")
+const load3 = new loadmore(items3, divNewArrive)
+load3.init()
+btNewArrive.addEventListener('click', function(){
+    load3.fnLoadItems()
+})
---
+++ script/module/loadmore.js
@@ -0,0 +1,31 @@
+export class loadmore {
+    constructor(items, element, range) {
+      this.items = items || [];
+      this.startIndex = 0;
+      this.ItemPerload = range || 4;
+      this.lastIndex = this.items.length;
+      this.parentElement = element
+    }
+
+    init() {
+        this.fnLoadItems()
+    }
+
+    fnLoadItems() {
+        let endIndex = this.startIndex + this.ItemPerload
+        for(let i = this.startIndex ; i < endIndex && i < this.lastIndex ; i++) {
+            this.fnCreateBox(this.parentElement, i)
+        }    
+        this.startIndex += this.ItemPerload;
+    }
+
+    fnCreateBox(div, index) {
+        const el = document.createElement("div");
+        const attr = document.createAttribute("class");    
+        attr.value = "box"; /* class box */
+        el.innerText = "index" + index;  
+        el.setAttributeNode(attr);
+        div.appendChild(el)
+    }
+    
+}
