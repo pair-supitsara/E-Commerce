dff23deb1e2448db132d6747369565dda4108040
pairsupit <pair.supitsara@gmail.com>
2023-08-20 21:39:16 +0700

    create class pagination
    for readable-reason, creating a class pagination

--- script/category.js
+++ script/category.js
@@ -30,56 +30,14 @@ btNewArrive.addEventListener('click', function(){
 })
 
 // pagination
+
+import { Pagination } from "./module/pagination.js"
+
 const itemsInCart = [0,1,2,3,4,5,6,7,8,9,10,11]
 const itemsPerPage = 5;
-let totalPage = Math.ceil(itemsInCart.length/itemsPerPage)
+const btPagination = document.getElementById("paginationButton")
+const parentCart = document.getElementById("items")
 
-let startIndex = 0
-let endIndex
-let pageNumber = 1
-
-fnLoadItemsPerPageInCart(pageNumber)
-function fnLoadItemsPerPageInCart(pageNumber) {
-    const parentCart = document.getElementById("items")
-    parentCart.innerHTML = ""
-    startIndex = (pageNumber-1) * itemsPerPage
-    endIndex = startIndex + itemsPerPage
-    for(let i=startIndex ; i<endIndex && i<itemsInCart.length ; i++){
-        fnCreateItemInCart(parentCart, i)
-    }
-}
-
-function fnCreateItemInCart(parentnode, index) {
-    const el = document.createElement("div")
-    el.classList.add("item")
-    const img = document.createElement("div")
-    img.classList.add("image")
-    el.appendChild(img)
-    const detail = document.createElement("div")
-    detail.innerText = index
-    detail.classList.add("detail-item")
-    el.appendChild(detail)
-    parentnode.appendChild(el)
-}
-
-initPaginiationButton(totalPage)
-function initPaginiationButton(totalPage) {
-    const btPagination = document.getElementById("paginationButton")
-    for(let i=0 ; i<totalPage ; i++){
-        fnCreatePaginationButton(btPagination, i)
-    }
-}
-function fnCreatePaginationButton(button, index) {
-    const span = document.createElement("span")
-    span.innerText = (index+1)
-    span.classList.add("pagination-item")
-    span.addEventListener('click', function() {
-        const allPageNumber = document.getElementsByClassName('pagination-item')
-        for(let item of allPageNumber){
-            item.classList.remove('pagination-item-active')
-        }
-        span.classList.add('pagination-item-active')
-        fnLoadItemsPerPageInCart(index+1)
-    })
-    button.appendChild(span)
-}
+const pagination = new Pagination(itemsInCart, itemsPerPage, parentCart, btPagination);
+pagination.fnInitItemsPerPageInCart()
+pagination.fnInitPaginiationButton()
---
+++ script/module/pagination.js
@@ -0,0 +1,65 @@
+export class Pagination {
+    constructor(items, itemsPerPage, cartItems, paginationButtons){
+        this.itemsInCart = items
+        this.itemsPerPage = itemsPerPage
+        this.NumItemsInCart = this.itemsInCart.length
+        this.totalPage = Math.ceil(this.NumItemsInCart / this.itemsPerPage)
+        this.startIndex = 0
+        this.endIndex = 0
+        this.pageNumber = 1
+
+        // parent element
+        this.cartItems = cartItems
+        this.paginationButtons = paginationButtons
+    }
+
+    fnInitItemsPerPageInCart() {
+        this.fnLoadItemsPerPageInCart(this.pageNumber)
+    }
+
+    fnLoadItemsPerPageInCart(pageNumber) {
+        this.cartItems.innerHTML = ''
+        this.startIndex = (pageNumber-1) * this.itemsPerPage
+        this.endIndex = this.startIndex + this.itemsPerPage
+        for(let i=this.startIndex ; i<this.endIndex && i<this.NumItemsInCart ; i++){
+            console.log("this.startIndex" + this.startIndex)
+            console.log("this.endIndex" + this.endIndex)
+            console.log(i)
+            this.fnCreateItemInCart(this.cartItems, i)
+        }
+    }
+
+    fnCreateItemInCart(parent, index) {
+        const html = `<div class='item'>
+                        <div class='image'></div>
+                        <div class='detail-item'>${index}</div>
+                    </div>`
+        parent.insertAdjacentHTML("beforeend", html);
+    }
+
+    fnInitPaginiationButton() {
+        for(let i=0 ; i<this.totalPage ; i++){
+            const pageNumber = (i+1)
+            this.fnCreatePaginationButton(this.paginationButtons, pageNumber)
+        }
+    }
+
+    fnCreatePaginationButton(parent, index) {
+        const span = document.createElement("span")
+        span.innerText = index
+        span.classList.add("pagination-item")
+        span.addEventListener('click', this.fnClickPaginationButton.bind(this))
+        parent.appendChild(span)
+    }
+
+    fnClickPaginationButton(event){
+        console.log(event.target.innerText)
+        const pageIndex = event.target.innerText;
+        const allPageNumber = document.getElementsByClassName('pagination-item')
+        for(let item of allPageNumber){
+            item.classList.remove('pagination-item-active')
+        }
+        event.target.classList.add('pagination-item-active')
+        this.fnLoadItemsPerPageInCart(pageIndex)
+    }
+}
