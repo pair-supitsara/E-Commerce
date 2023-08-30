de9376b0c0b9b5dc4c8323969701a48eb205a8f5
pairsupit <pair.supitsara@gmail.com>
2023-08-30 23:49:18 +0700

    add css modal

--- script/login.js
+++ script/login.js
@@ -5,30 +5,40 @@ import modal from './module/modal.js'
 // #region event listener
 const backdrop = document.getElementById("backdrop")
 const alertModal = document.getElementById("alertModal")
+backdrop.addEventListener('click', function(event){
+    if(!event.target.closest('.card') // click backdrop always close modal
+        || event.target.closest('.x-square-icon')) { // click [x] always close modal
+        modal.fnHideModal(backdrop)
+    }
+})
 
 const btnLogin = document.getElementById("btnLogin")
-
 btnLogin.addEventListener('click', async function(event){
     event.preventDefault()
     const inputEmailLogin = document.getElementById("inputEmailLogin").value
     const inputPasswordLogin = document.getElementById("inputPasswordLogin").value
-    modal.fnShowmodal(backdrop, alertModal, 'visible')
-    // await fnAuthen(inputEmailLogin, inputPasswordLogin)
+    await fnAuthen(inputEmailLogin, inputPasswordLogin)
 })
 
 // #endregion
 async function fnAuthen(email, password) {
+    const alertMsg = document.getElementById("alertMsg")
+
     const isValidEmail = validate.fnValidationEmail(email)
     let msg = ""
     if(!isValidEmail) {
-        msg += "isValidEmail"
+        msg += `<li style="margin-left: 35px;">email</li>`
     }
     const isValidPassword = validate.fnValidationPassword(password)
+
     if(!isValidPassword) {
-        msg += "isValidPassword"
+        msg += `<li style="margin-left: 35px;">password</li>`
+        console.log(msg)
     }
     if(msg.length > 0) {
-        alert(msg)
+        alertMsg.innerHTML = `  <div style="">Please enter valid following</div>
+                                <ul style="">${msg}</ul>`
+        modal.fnShowmodal(backdrop, alertModal, 'visible')
     } else {
         const json = {
             email: email,
